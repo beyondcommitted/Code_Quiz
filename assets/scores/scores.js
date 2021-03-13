ac5a31b7dd8add6ea3476ae4131c9edb39bd8e10
Breon Sparrow <beyond.committed@gmail.com>
2021-03-13 15:00:52 -0700

    Buttons work

--- assets/scores/scores.js
+++ assets/scores/scores.js
@@ -2,18 +2,25 @@
 var score = document.querySelector("#score");
 var clear = document.querySelector("#clear");
 
-// Clears scores 
+// Clears scores
 clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
+  localStorage.clear();
+  location.reload();
 });
-// // Grabs the initials and scores from the local storage 
-// // var quizScores = localStorage.getItem("quizScores");
-// // quizScores = JSON.parse(quizScores);
-// // if (quizScores !== null) {
-    //var createListItem = document.createElement("li");
-// // createListItem.textContent = quizScores[i].initials + 
-    //" " + quizScores[i].score; score.append(createListItem);
+// Grabs the initials and scores from the local storage
+var quizScores = localStorage.getItem("quizScores");
+quizScores = JSON.parse(quizScores);
+console.log(quizScores);
+if (quizScores !== null) {
+  for (let i = 0; i < quizScores.length; i++) {
+    var createListItem = document.createElement("li");
+    createListItem.textContent =
+      quizScores[i].initials + " " + quizScores[i].userScore;
+    score.append(createListItem);
+  }
+};
 
-//     }
-// }
+document.getElementById('returnBack').addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.replace("../../index.html");
+  });
